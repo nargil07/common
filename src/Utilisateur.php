d976204a0b145d6f0bd12605ef98bbdc36b8cd98
nargil07 <nargilcorp@gmail.com>
2016-09-12 16:50:47 +0200

    m

--- src/Utilisateur.php
+++ src/Utilisateur.php
@@ -165,7 +165,7 @@ class Utilisateur extends AbstractModel implements Authenticatable
     }
 
     public function Personne(){
-        return $this->belongsTo('Ciss\Personne', 'IDPersonne', 'IDPersonne');
+        return Personne::where('IDPersonne', $this->IDPersonne)->first();
     }
 
 
